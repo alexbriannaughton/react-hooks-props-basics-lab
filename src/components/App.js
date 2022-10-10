22f8c7e4521fab7a37a743f21e64c6e68d7fb90a
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-10-10 11:14:46 -0700

    commit tree 8dc195e5cefdbc6bc8abc560a3bc9df5c36db70f

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} name={user.name} city={user.city}/>
+      <About github={user.links.github} bio={user.bio} linkedin={user.links.linkedin}/>
     </div>
   );
 }
